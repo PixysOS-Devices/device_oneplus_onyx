52e8114774f4d7c1499bb4be16c924ae20021cb4
nikhilgohil <nikhil.gohil3@gmail.com>
2020-03-02 13:17:41 +0530

    Pixys-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_onyx.mk
+    $(LOCAL_DIR)/pixys_onyx.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_onyx-user \
-    lineage_onyx-userdebug \
-    lineage_onyx-eng
+    pixys_onyx-user \
+    pixys_onyx-userdebug \
+    pixys_onyx-eng
